62a61ecbf9feb1d0548972ca534d14044cf24600
Julien Nakache <julien.nakache@gmail.com>
2023-05-11 18:56:29 -0400

    clean up

--- vocode/streaming/models/worker.py
+++ vocode/streaming/models/worker.py
@@ -1,5 +1,6 @@
 import asyncio
 import threading
+import collections
 import janus
 
 
@@ -27,10 +28,69 @@ class AsyncWorker:
         return False
 
 
-# class InterruptibleAsyncWorker(AsyncWorker):
+class InterruptibleEvent:
+    def __init__(
+        self,
+        is_interruptible,
+        payload=None,
+    ):
+        self.interruption_event = threading.Event()
+        self.is_interruptible = is_interruptible
+        self.payload = payload
+
+    def interrupt(self):
+        return False if self.is_interruptible else self.interruption_event.set()
+
+    def is_interrupted(self):
+        return self.is_interruptible and self.interruption_event.is_set()
 
 
-class ThreadAsyncWorker(AsyncWorker):
+class IterruptipleWorker(AsyncWorker):
+        def __init__(
+        self,
+        input_queue: asyncio.Queue,
+        output_queue: asyncio.Queue,
+        max_concurrency=2,
+    ) -> None:
+        super().__init__(input_queue, output_queue)
+        self.max_concurrency = max_concurrency
+
+    async def _run_loop(self):
+        # TODO(julien) Implement concurrency with max_nb_of_thread
+        try:
+            while True:
+                item = await self.input_queue.get()
+                if isinstance(item, InterruptibleEvent) and item.is_interrupted():
+                    continue
+                self.interuptible_event = item
+                self.current_task = asyncio.create_task(
+                    self.process(item, self.output_queue)
+                )
+                await self.current_task
+                self.current_task = None
+        except asyncio.CancelledError:
+            pass
+
+    async def process(self, item):
+        """
+        Publish results onto output queue.
+        Calls to async function / task should be able to handle asyncio.CancelledError gracefully:
+        """
+        raise NotImplementedError
+
+    def cancel_current_task(self):
+        """Free up the resources. That's useful so implementors do not have to implement this but:
+        - threads tasks won't be able to be interrupted. Hopefully not too much of a big deal
+            Threads will also get a reference to the interuptible event
+        - asyncio tasks will still have to handle CancelledError and clean up resources
+        """
+        if self.current_task and self.interuptible_event.is_interruptible:
+            return self.current_task.cancel()
+
+        return False
+
+
+class ThreadAsyncWorker(IterruptipleWorker):
     """
     This would be the synthesizer
     """
@@ -47,86 +107,24 @@ class ThreadAsyncWorker(AsyncWorker):
         super().__init__(input_queue, output_queue)
         self.max_nb_of_thread = max_nb_of_thread
         self.blocking_task = blocking_task
-        self.output_janus_queue = janus.Queue
 
-    def start(self) -> asyncio.Task:
-        self.current_task = asyncio.create_task(self.run_loop())
 
-    async def _run_loop(self):
-        # TODO(julien) Implement concurrency
-        while True:
-            item = await self.input_queue.get()
-            self.current_task = asyncio.to_thread(
-                self.blocking_task, self.output_janus_queue.sync_q, *item
-            )
-            self.forward_task = asyncio.create_task(
-                self._forward_from_thead(self.janus_output_queue)
-            )
-            await self.current_task
-            self.output_janus_queue.async_q.put_nowait(self._EOQ)
-            await self.forward_task
+    async def process(self, item, output_queue):
+        output_janus_queue = janus.Queue
+        thread_task = asyncio.to_thread(
+            self.blocking_task, output_janus_queue.sync_q, *item
+        )
+        forward_task = asyncio.create_task(self._forward_from_thead(janus_output_queue, output_queue))
+        await thread_task
+        output_janus_queue.async_q.put_nowait(self._EOQ)
+        await forward_task
 
-            try:
-                item = await self.current_task
-            except asyncio.CancelledError:
-                pass
-
-    async def _forward_from_thead(self, output_janus_queue):
+    async def _forward_from_thead(self, output_janus_queue, output_queue):
         while True:
             thread_item = await output_janus_queue.async_q.get()
             if self._EOQ:
                 return
-            self.output_queue.put_nowait(thread_item)
+            output_queue.put_nowait(thread_item)
 
-
-class SimpleQueueWorker(AsyncWorker):
-    def __init__(self, input_queue: asyncio.Queue, output_queue: asyncio.Queue) -> None:
-        super().__init__(input_queue, output_queue)
-        self.current_task: None | asyncio.Task = None
-
-    async def run_loop(self):
-        while True:
-            item = await self.input_queue.get()
-            self.current_task = asyncio.create_task(self.process(item))
-            try:
-                await self.current_task
-            except asyncio.CancelledError:
-                # This is a good place to do something if needed
-                # Ex: update the cut_off message
-                pass
-
-    async def process(self, item):
-        """
-        Publish results onto output queue.
-        Calls to async function / task should be able to handle
-        asyncio.CancelledError gracefully:
-            - Ideally, you would want to make sure that async tasks await-ed
-              down the call stack are also cancelled
-              and are also handling CancelledError gracefully.
-            - When making async aiohttp calls, things should be ok, but it's
-              always best to add tests for that.
-        TODO(julien) Add function to recursively cancel and patterns to test
-            cancellation
-        TODO(julien) Do we want to abstract away the output queue so it only process stuff?
-            Let's see once we implement a couple
-        """
+    def blocking_task(self, output_queue, *args):
         raise NotImplementedError
-
-    def cancel_current_task(self):
-        if self.current_task and not self.current_task.done():
-            return self.current_task.cancel()
-
-        return False
-
-    async def _cancel_recursively(self, task: asyncio.Task):
-        # TODO(julien) Do we want to use this or something like hiarchechical cancellation such any.IO?
-        # https://stackoverflow.com/questions/70596654/asyncio-automatic-cancellation-of-subtasks
-        if not task.done():
-            task.cancel()
-            try:
-                await task
-            except asyncio.CancelledError:
-                pass
-
-        if hasattr(task, "_fut_waiter") and not task._fut_waiter.done():
-            await self._cancel_recursively(task._fut_waiter)
